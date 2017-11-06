fd7866e8a0b181cd6b0260b30434ce36a7f04749
nickc <nickc@10.1.81.174>
2017-11-06 12:56:54 -0800

    @toropez @shayromayo

--- src/BinaryHeap.java
+++ src/BinaryHeap.java
@@ -1,3 +1,4 @@
+
 import java.util.NoSuchElementException;
 
 public class BinaryHeap {
@@ -25,40 +26,36 @@ public class BinaryHeap {
 		arr[parent] = temp;
 	}
 
-	private void growHeap() {
+	public void growHeap() {
 		// TODO Auto-generated method stub
 		int[] newArr = new int[arr.length * 2];
 		System.arraycopy(arr, 0, newArr, 0, arr.length);
 		arr = newArr;
 	}
+	
 
 	public int remove() {
 		// TODO Auto-generated method stub
-		if(arr.length == 0){  
+		if(arr.length <= 0){  
 			throw new NoSuchElementException();
 		}
 		int removed = arr[0];
 		arr[0] = arr[--size];
 		int index = 0;
 		int child = leftChild(index);
-		if(arr[rightChild(index)] < arr[child] && child < size){
+		if(child < 0 || child > size - 1)
+			return removed;
+		if(rightChild(index) < size - 1 && arr[rightChild(index)] < arr[child]){ 
 			child = rightChild(index);
 		}
 		
-		while(arr[child] < arr[index] && child < size){		
-			swap(index, child);
-			index = child;
+		while( child < size && arr[child] < arr[index]){			
+			swap(index, child);			
+ 			index = child;
 			child = leftChild(index);
-			if(rightChild(index) < size && arr[rightChild(index)] < arr[index]){
+			if(rightChild(index) < size && arr[rightChild(index)] < arr[child]){ //child?
 				child = rightChild(index);
 			}
-			
-			System.out.println("Arr child: " + arr[child]);
-			System.out.println("arr[index]: " + arr[index]);
-			System.out.println("Child: " + child + " - size: " + size);
-			System.out.println();
-			
-			
 			
 		}
 		
@@ -84,3 +81,4 @@ public class BinaryHeap {
 	}
 	
 }
+
